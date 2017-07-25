284fb8d67d65263f718fe0f0979af47703f91224
Tomzhang12138 <1316727932@22.com>
2017-07-25 09:57:39 +0800

    demo commit

---
+++ src/components/app.vue
@@ -0,0 +1,30 @@
+<template>
+<vue-datetime-picker ref:picker1 name="picker1"
+                                  :model="datetime1" type="datetime"
+                                  language="en-US"
+                                  datetime-format="YYYY-MM-DD HH:mm:ss">
+          </vue-datetime-picker>
+
+</template>
+<script>
+
+import datetimePicker from './datetime-picker';
+   export default {
+            components: {
+                "vue-datetime-picker": datetimePicker
+            },
+            data() {
+                return {
+                    datetime1: new Date(),
+                    startDatetime: moment(),
+                    endDatetime: null
+                };
+            },
+            methods: {
+                updateDate: function(datetime) {
+                    this.result1 = datetime;
+                }
+            }
+
+        };
+</script>
